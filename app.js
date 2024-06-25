91465fd3a4aa5671141dfeebc1f802b58e0ad2ed
sourav <ss7965418@gmail.com>
2024-06-25 14:14:40 +0530

    added no city in app.js

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-import express from "express"
-import axios from "axios"
+import express from "express";
+import axios from "axios";
 import bodyParser from "body-parser";
 
-const app=express();
-const port=3000;
+const app = express();
+const port = 3000;
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
- app.use(express.static('public'));
- app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    res.render("index",{weather:""});
-})
- let city='',country='';
-
-app.post('/weather',async (req,res)=>{
-   // take input from ejs file for the country and city 
-       city=req.body.city;
-       country=req.body.country;
-    // take data of the place from the api call   
-       const weat=`https://api.weatherbit.io/v2.0/current?city=${city}&country=${country}&key=e142737447ea46a991b340a45a4225bf`
-
-  try{
-    const response=await axios.get(weat);
-    // console.log(response.data);
-
-    
-        const data = response.data.data[0];
-        console.log(data);
-     
-    
-        res.render('index', { weather:data });
-   
-
-} catch (error) {
-    console.error("Error fetching the weather data:", error);
-    res.render('index', { weather: null });
-}
+app.get('/', (req, res) => {
+    res.render("index", { weather: '', city_exist: null });
 });
- 
-app.listen(port,(req,res)=>{
+
+let city = '', country = '';
+
+app.post('/weather', async (req, res) => {
+    // take input from ejs file for the country and city 
+    city = req.body.city;
+    country = req.body.country;
+    // take data of the place from the api call   
+    const weat = `https://api.weatherbit.io/v2.0/current?city=${city}&country=${country}&key=e142737447ea46a991b340a45a4225bf`
+
+    try {
+        const response = await axios.get(weat);
+
+        const data = response.data.data[0];
+        const city_exist = data ? data.lat : 0;
+        console.log(city_exist);
+
+        if (city_exist === 0) {
+            res.render("index.ejs", { weather: '', city_exist: 0 });
+        } else {
+            res.render('index', { weather: data, city_exist: 1 });
+        }
+    } catch (error) {
+        console.error("Error fetching the weather data:", error);
+        res.render('index', { weather: null, city_exist: 0 });
+    }
+});
+
+app.listen(port, () => {
     console.log((`app listening on the port ${port}`));
-}) 
+});
